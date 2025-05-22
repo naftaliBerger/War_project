4d146349f00c1b364e526b7d70b0020fcee432ec
Naftali Berger <bn26665@gmail.com>
2025-05-22 10:36:01 +0300

    add class terrorist and report updated

--- War_project/classes/Report.cs
+++ War_project/classes/Report.cs
@@ -1,1 +1,14 @@
-﻿
+﻿public class Report
+{
+    Terrorist terrorist;
+    string Location;
+    string time;
+
+    public Report(Terrorist terrorist)
+    {
+        this.terrorist = terrorist;
+    }
+
+
+    
+}
