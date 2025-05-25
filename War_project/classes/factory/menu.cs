099bdb124c0a30af453e8e67c53dc776f299cfc6
Naftali Berger <bn26665@gmail.com>
2025-05-25 15:47:32 +0300

    Adding a management function

---
+++ War_project/classes/factory/menu.cs
@@ -0,0 +1,18 @@
+﻿public static class Menu
+{
+    public static void menu(int num)
+    {
+        bool 
+        while (true)
+        {
+            switch (num)
+            {
+                case 0:
+                    {
+                        exit();
+                    }
+
+            }
+        }
+    }
+}
